1b5bcf60b84d92b95dd9f9dbff1289a72b305bec
YidaozhanYa <73830635+YidaozhanYa@users.noreply.github.com>
2021-10-09 19:14:20 +0800

    Update enable-seewo-service.sh

--- enable-seewo-service.sh
+++ enable-seewo-service.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export DISPLAY=:0
 my_user_name=`who | cut -d' ' -f1 | sort | uniq | grep -v "root"`
 echo "${my_user_name}"
 runuser -u ${my_user_name} -- "/opt/apps/com.seewo.seewoservice/files/seewoassistant/SeewoServiceAssistant-linux-x64/SeewoServiceAssistant" &
