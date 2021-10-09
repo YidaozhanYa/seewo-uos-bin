b0f5a45803930cdb35536e9565555337d8aa8733
YidaozhanYa <73830635+YidaozhanYa@users.noreply.github.com>
2021-10-09 19:13:47 +0800

    Add files via upload

---
+++ enable-seewo-service.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+my_user_name=`who | cut -d' ' -f1 | sort | uniq | grep -v "root"`
+echo "${my_user_name}"
+runuser -u ${my_user_name} -- "/opt/apps/com.seewo.seewoservice/files/seewoassistant/SeewoServiceAssistant-linux-x64/SeewoServiceAssistant" &
+ps -aux | grep seewoservice &
